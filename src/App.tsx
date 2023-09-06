58a6d315a13bfdebeeb700e571d1f232c12b259e
Hamza J <104732125+HJ1111111@users.noreply.github.com>
2023-09-06 16:31:52 +0200

    nova-components package added, .npmrc file with token created

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './nova.css'
+import {NvStack, NvButton } from '@eliagroup-innersource/nova-components-react/';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+<NvStack gutter={4} mb={4} overflow="auto">
+  <NvButton color="neutral"> Neutral </NvButton>
+  <NvButton color="primary"> Primary </NvButton>
+  <NvButton color="secondary"> Secondary </NvButton>
+  <NvButton color="error"> Error </NvButton>
+</NvStack>
   )
 }
 
