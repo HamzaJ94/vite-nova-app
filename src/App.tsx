244f6f02fb919fa565a281c2d94f7618ab1076aa
Hamza J <104732125+HJ1111111@users.noreply.github.com>
2023-09-13 17:03:41 +0200

    table and button were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './nova.css'
-import {NvStack, NvFieldText, NvText, NvAvatar } from '@eliagroup-innersource/nova-components-react/';
+import {NvStack, NvFieldText, NvText, NvAvatar, NvFieldSelect, NvOption, NvFieldTextArea, NvList, NvListItem, NvTable, NvTableRow, NvTableCell, NvButton } from '@eliagroup-innersource/nova-components-react/';
 
 function App() {
 
@@ -33,6 +33,54 @@ function App() {
 </NvStack>
 &nbsp;
 <div className='hrTag'></div>
+
+<NvStack gutter={4} margin="5 0 0 0" padding={3}>
+<NvFieldSelect className="labelSamplefeed" label="How did you hear about us?"  required  margin=". . 3">
+  <NvOption value="value-1"> label 1 </NvOption>
+  <NvOption value="value-2"> label 2 </NvOption>
+  <NvOption value="value-3"> label 3 </NvOption>
+</NvFieldSelect>
+<NvFieldText className="labelSamplespecify" label="Please Specify" required/>
+</NvStack>
+
+<NvFieldTextArea margin="5 0 0 0" padding={3} className="LabelSamplefeedback" label="Feedback about us:"></NvFieldTextArea>
+<NvFieldTextArea margin="5 0 0 0" padding={3} className="LabelSamplefeedback" label="Suggestions if any for further improvment:"></NvFieldTextArea>
+<NvText margin="5 0 0 0" padding={3}>Will you be willing to recommend us?</NvText>
+
+<NvList margin="0 0 0 5" padding={3} bullet>
+  <NvListItem href="#" isActive> Yes </NvListItem>
+  <NvListItem href="#"> No </NvListItem>
+  <NvListItem href="#"> Maybe </NvListItem>
+</NvList>
+
+<NvText margin="4 0 0 0" padding={3}>Please give reference of any two people whom you feel:</NvText>
+
+<NvTable className='table' margin="1 0 0 0" padding={1} striped>
+  
+  <NvTableRow slot="header">
+    <NvTableCell grid-cols="6">  </NvTableCell>
+    <NvTableCell grid-cols="6"> Full Name </NvTableCell>
+    <NvTableCell grid-cols="6"> Address </NvTableCell>
+    <NvTableCell grid-cols="6"> Contact Number </NvTableCell>
+  </NvTableRow>
+  <NvTableRow>
+    <NvTableCell grid-cols="6"> 1 </NvTableCell>
+    <NvTableCell grid-cols="6">  </NvTableCell>
+    <NvTableCell grid-cols="6">  </NvTableCell>
+    <NvTableCell grid-cols="6">  </NvTableCell>
+  </NvTableRow>
+  <NvTableRow>
+    <NvTableCell grid-cols="6"> 2 </NvTableCell>
+    <NvTableCell grid-cols="6">  </NvTableCell>
+    <NvTableCell grid-cols="6">  </NvTableCell>
+    <NvTableCell grid-cols="6">  </NvTableCell>
+  </NvTableRow>
+
+</NvTable>
+<br/>
+
+<NvButton className="submitbtn"> Submit </NvButton>
+
 </div>
   )
 }
